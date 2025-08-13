93c5781fadbd1c65004d972e86f70008c556b588
pedrots <pedrotorres9843@gmail.com>
2025-08-12 22:09:09 -0300

    Casa - 12/08/2025

--- sessão 5/form/src/components/MyForm.jsx
+++ sessão 5/form/src/components/MyForm.jsx
@@ -1,38 +1,75 @@
+import { use, useState } from "react";
+import "./MyForm.css";
 
-import { useState } from 'react'
-import './MyForm.css'
+const MyForm = ({ user }) => {
+  // manipulação de dados/onchance
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
+  const [bio, setBio] = useState("");
+  const [role, setRole] = useState("");
 
-const MyForm = ({user}) => {
-    // manipulação de dados/onchance
-    const [name, setName] = useState(user ? (user.name) : (""));
-    const [email, setEmail] = useState(user ? (user.email) : (""));
-
-    const handleName = (e) =>{
-        setName(e.target.value)
-        // console.log(name)
-    }
-    const handleSubmit = (e)=> {
-        e.preventDeafult(); 
-    }
+  // funções
+  const handleName = (e) => {
+    setName(e.target.value);
+    console.log(name);
+  };
+  const handleSubmit = (e) => {
+    //Ação do form
+    e.preventDefault();
+    // Limpar formulario
+    setName("");
+    setEmail("");
+    setBio("");
+    setRole("");
+  };
   return (
     <div>
       {/*  criação de form */}
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
-            {/* label com html for */}
-            <label htmlFor="name">Nome:</label>
-            <input type="text" name='name' placeholder='Digite seu nome aqui' onChange={handleName}/>
+          {/* label com html for */}
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome aqui"
+            onChange={handleName}
+            value={name}
+          />
 
-            {/* Label envolvendo input */}
-            <label>
-                <span>E-mail:</span>
-                <input type="email" name='email' placeholder='Digite seu email aqui' onChange={(e) => setEmail(e.target.value)}/>
-            </label>
+          {/* Label envolvendo input */}
+          <label>
+            <span>E-mail:</span>
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite seu email aqui"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <span>Biografia do usuario</span>
+            <textarea
+              name="bio"
+              placeholder="Digite sua descrição aqui"
+              onChange={(e) => setBio(e.target.value)}
+              value={bio}
+            ></textarea>
+          </label>
+          <label>
+            <span>Função no sistema</span>
+            <select name="role" onChange={(e)=> setRole(e.target.value)} value={role}>
+              <option value="user">Usuário</option>
+              <option value="editor">Editor</option>
+              <option value="Adimin">Adiministrador</option>
+            </select>
+          </label>
         </div>
-        <input type="submit" value="Enviar"/>
-       </form>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
--- sessão 6/secretword/src/App.jsx
+++ sessão 6/secretword/src/App.jsx
@@ -1,19 +1,19 @@
 // Css
-import './App.css'
+import "./App.css";
 // React
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 // data
-import {wordsList} from '../src/data/words';
+import { wordsList } from "../src/data/words";
 // Componentes
-import StartScreen from './components/StartScreen'
-import Game from './components/Game'
-import GameOver from './components/GameOver'
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 // Criação de telas
-  const stages = [
-    {id:1, name:"start"},
-    {id:2, name:"game"},
-    {id:3, name:"end"}
-  ];
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
@@ -23,30 +23,37 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
-  const pickWordAndCategory = () =>{
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = () => {
     // pick a random category
-    const categories = Object.keys(words)
+    const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * categories.length)];
 
     // pick a random word
-    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
 
     // return word and category
-    return {word, category}
+    return { word, category };
 
     // console.log(`categoria: ${category} - palavra: ${word}`); //teste de categoria e palavra
-  }
-
+  };
 
   // start Secret Word
-  const startGame = () =>{
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLetterStates();
     // pick word and pick category
-    const {word, category} = pickWordAndCategory();
+    const { word, category } = pickWordAndCategory();
     // console.log(word, category) //show the category and words of a function
 
     // create an array of letters
     let wordLetters = word.split("");
-    wordLetters = wordLetters.map((l) => l.toLowerCase()) //trasform all letters in lower case letters
+    wordLetters = wordLetters.map((l) => l.toLowerCase()); //trasform all letters in lower case letters
     // console.log(wordLetters) //test the letters separation
 
     // fill useState's
@@ -56,24 +63,82 @@ function App() {
 
     // go to game screen
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
   // process the letter input
-  const verifyLetter = () =>{
-    setGameStage(stages[2].name);
-  }
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+    // push letter in your group(wrong or right) && remove or no a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+  // Check if guesses ended
+  useEffect(() => {
+        if (guesses <= 0) {
+          // reset all states
+          clearLetterStates();
+          setGameStage(stages[2].name);
+        }
+      }, [guesses]);
+  // Check win condition
+  useEffect(()=>{
+    const uniqueLetters = [... new Set(letters)]
+
+    if(guessedLetters.length == uniqueLetters.length){
+      // add score
+      setScore((actualScore) => actualScore += 100);
+      // restart game new word
+      startGame();
+    }
+  }, [guessedLetters])
+  
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
   // Restarts the game
   const retry = () => {
+    setScore(0);
+    setGuesses(3);
     setGameStage(stages[0].name);
-  }
+  };
 
   return (
-      <div className='App'>
-        {gameStage === "start" && <StartScreen startGame={startGame}/>}
-        {gameStage === "game" && <Game verifyLetter={verifyLetter}/> }
-        {gameStage === "end" && <GameOver retry={retry}/>}
-      
-      </div>
-  )
+    <div className="App">
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
+    </div>
+  );
 }
 
-export default App
+
+export default App;
